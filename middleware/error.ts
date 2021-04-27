3a0f9627749eade1b86f9ad2cda40e5e6b9cb5b0
Maciej Pędzich <dnalop.me@gmail.com>
2021-04-27 21:11:26 +0200

    Add special keys for duplicate key error

--- middleware/error.ts
+++ middleware/error.ts
@@ -1,4 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
+import { QueryFailedError } from 'typeorm/error/QueryFailedError';
 import BaseHttpError from '../errors/base-http';
 
 export default function errorMiddleware(
@@ -7,13 +8,25 @@ export default function errorMiddleware(
   res: Response,
   next: NextFunction
 ) {
+  if (process.env.NODE_ENV === 'development') {
+    console.error(error);
+  }
+
   let status = 500;
   let message = 'Unexpected error occurred';
 
   if (error instanceof BaseHttpError) {
     status = error.status;
     message = error.message;
+  } else if (
+    error instanceof QueryFailedError &&
+    error.message.includes('duplicate key value')
+  ) {
+    status = 409;
+    message = 'This email address has already been registered';
   }
+
+  console.log();
 
   return res.status(status).json({ error: message });
 }
